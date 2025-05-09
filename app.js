9cce489a8e2ce1569d2f1cc614aa73ad46203190
rjohal164 <r.johal164@gmail.com>
2025-05-08 19:10:25 -0700

    Installed Prompt-sync and did some initial learnings up to reading

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Imports at the top of your file
-const { log } = require("console");
 const path = require("path");
+const fs = require("fs");
 
+// Path Example
 const filePath = "/users/test/file.txt";
 
 console.log(`Dirname: ${path.dirname(filePath)}`);
@@ -10,3 +11,23 @@ console.log(`Extension: ${path.extname(filePath)}`);
 
 const fullPath = path.join("user", "test", "text.md");
 console.log(`FullPath: ${fullPath}`);
+
+// FS Example
+const filePathHello = path.join(__dirname, "hello.txt");
+const helloContent = "Hello from Node.js";
+
+try {
+  fs.writeFileSync(filePathHello, helloContent);
+
+  console.log(`Succesffuly wrote to ${filePathHello}`);
+} catch (error) {
+  console.error(`Error ${filePathHello}`);
+}
+
+// Reading Data
+try {
+  const data = fs.readFileSync(filePathHello);
+  console.log(`data inside file: ${data}`);
+} catch (error) {
+  console.error(`Error reading file ${filePathHello}`);
+}
